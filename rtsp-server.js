966a53085cfd59bad9cbeddda72f127d987af74e
Larry Poon <lpoon2@illinois.du>
2016-11-02 20:34:10 -0500

    rtsp

--- rtsp-server.js
+++ rtsp-server.js
@@ -9,20 +9,34 @@ var methodOverride = require('method-override');
 var port = process.env.PORT || 8080;
 var session      = require('express-session');
 var fs = require('fs');
+const url = require('url');
+//var transform = require('sdp-transform');
 
-var rtsp = require('rtsp-server')
+var rtsp = require('rtsp-server');
 
 function requestHandler(req, res){
-  console.log(req.method, req.url)
-   
+  //  console.log(req);
+    console.log(res);
+     var url_obj = url.parse(req.uri);
+  //var url_obj  = transform.parse(sdpStr);
+
+  console.log(req.method, url_obj);
+    url_obj.session = {
+        //TODO: put session information
+    };
     switch (req.method) {
       case 'OPTIONS':
-        res.setHeader('Public', 'OPTIONS')
-        break
+        res.setHeader('Public', ['DESCRIBE','SETUP','TEARDOWN','PLAY','PAUSE']);
+        res.statusCode = 200;
+        break;
+      case 'ANNOUNCE':
+        res.statusCode = 200;
+        console.log("announce!");
       default:
         res.statusCode = 501 // Not implemented 
     }
-    res.end('hi there'); // will echo the CSeq header used in the request 
+   // res.end('hi there'); // will echo the CSeq header used in the request 
+    res.end();
 }
 var server = rtsp.createServer(requestHandler);
  
